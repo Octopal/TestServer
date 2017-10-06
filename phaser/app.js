a3247b19ffad6709d3a05f58ea9986d096225d2b
Octopal <roldan@thecosmicwhale.com>
2017-10-06 15:38:17 -0400

    change sprite

--- phaser/app.js
+++ phaser/app.js
@@ -13,8 +13,8 @@ app.get('/',function(req,res){
 
 server.lastPlayderID = 0;
 
-server.listen(process.env.PORT || 8081,function(){
-    console.log('Listening on '+server.address().port);
+server.listen(process.env.PORT || 3000), function(){
+    console.log('Listening on '+ server.address().port);
 });
 
 io.on('connection',function(socket){
