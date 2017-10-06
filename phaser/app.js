43c477ab2da079309343776fef8e00285287cfa0
Octopal <roldan@thecosmicwhale.com>
2017-10-06 15:44:58 -0400

    change port to 8080

--- phaser/app.js
+++ phaser/app.js
@@ -13,7 +13,7 @@ app.get('/',function(req,res){
 
 server.lastPlayderID = 0;
 
-server.listen(process.env.PORT || 3000), function(){
+server.listen(process.env.PORT || 8080), function(){
     console.log('Listening on '+ server.address().port);
 };
 
